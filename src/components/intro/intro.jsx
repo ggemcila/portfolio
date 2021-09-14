03042d21a478cfe0cd498eec04b1ee26605c7f05
Gemcila <ggemcila@gmail.com>
2021-09-14 12:28:21 +0100

    #5Components

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,8 +1,18 @@
 import "./intro.scss";
 export default function intro() {
     return (
-        <div className="intro">
-            
+        <div className="intro" id="intro">
+            <div className="titleIntro">
+                Hi, I'm Gemcila! I believe 
+            </div>
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left"></div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,10 @@ import "./topbar.scss";
 export default function Topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+               
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
