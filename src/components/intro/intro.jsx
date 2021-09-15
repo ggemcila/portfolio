49bda64a1775053a3e71bec6874b6177252b29da
Gemcila <ggemcila@gmail.com>
2021-09-15 16:02:53 +0100

    #7 useState(0)

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,42 +1,51 @@
 import "./intro.scss";
 export default function intro() {
 
-    const data =[
-        {id:"1",
-        title: "Quiz app",
-        img: "./assets/bombhome.png",
-    },
-    {id:"2",
-        title: "Weather app",
-        img: "./assets/bombabout.png",
-    },
-    {id:"3",
-        title: "Movie review page",
-        img: "./assets/bombaccount.png",
-    },
-];
+    const[currentSlide,setCurrentSlide] =  useState(0);
+    const data = [
+        {
+            id: "1",
+            title: "Quiz app",
+            img: "./assets/bombhome.png",
+        },
+        {
+            id: "2",
+            title: "Weather app",
+            img: "./assets/bombabout.png",
+        },
+        {
+            id: "3",
+            title: "Landing page",
+            img: "./assets/bombaccount.png",
+        },
+        {
+            id: "4",
+            title: "Movie review page",
+            img: "./assets/bombaccount.png",
+        },
+    ];
     return (
         <div className="intro" id="intro">
             <div className="titleIntro">
                 Hi I'm Gemcila, Junior Web Developer who believes that aesthetically pleasing design is more usable, creates a positive response and encourages participation.
             </div>
-            <div className="slider">
-            {data.map((d) => (  
-                <div className="container">
-                    <div className="item">
-                        <div className="left"></div>
-                        <div className="leftContainer">
-                            <div className="imgContainer">
-                                <img src={d.img} alt="" className="imghome" />
+            <div className="slider" style={{transform:`translateX(-${currentSlide*100}vw)`}}>
+                {data.map((d) => (
+                    <div className="container">
+                        <div className="item">
+                            <div className="left"></div>
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src={d.img} alt="" className="imghome" />
+                                </div>
                             </div>
+                            <div className="right"></div>
                         </div>
-                        <div className="right"></div>
                     </div>
-                </div>
                 ))}
             </div>
             <img src="assets/arrow.png" alt="" className="arrow left" />
-            <img src="assets/arrow.png" alt="" className="arrow right" />
+            <img src="assets/arrow.png" alt="" className="arrow right"  />
         </div>
     )
 }
