f115d6d1914db45569d6b37661475b2b5345e6e1
Gemcila <ggemcila@gmail.com>
2021-09-15 15:42:14 +0100

    #7 Defining data in intro.js

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,9 +1,24 @@
 import "./intro.scss";
 export default function intro() {
+    
+    const data =[
+        {id:"1",
+        title: "Quiz app",
+        img: "./assets/bombhome.png",
+    },
+    {id:"2",
+        title: "Weather app",
+        img: "./assets/bombabout.png",
+    },
+    {id:"3",
+        title: "Movie review page",
+        img: "./assets/bombaccount.png",
+    },
+];
     return (
         <div className="intro" id="intro">
             <div className="titleIntro">
-                Hi I'm Gemcila, Junior Web Developer who believes that aesthetically pleasing design is more usable, creates a positive response and encourages participation
+                Hi I'm Gemcila, Junior Web Developer who believes that aesthetically pleasing design is more usable, creates a positive response and encourages participation.
             </div>
             <div className="slider">
                 <div className="container">
