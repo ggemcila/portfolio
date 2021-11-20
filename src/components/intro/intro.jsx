99cde3f62fb56765b3639494e341f06951d12322
Gemcila <ggemcila@gmail.com>
2021-11-20 17:24:29 +0000

    added images

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,4 +1,6 @@
 import "./intro.scss";
+import bomblogo from "../../assets/bomblogo.png";
+import weathertoday from "../../assets/weathertoday.png";
 
 const Intro = () => {
     return (
@@ -6,6 +8,9 @@ const Intro = () => {
             <div className="titleIntro">
                 Hi I'm Gemcila, Junior Web Developer who believes that aesthetically pleasing design is more usable, creates a positive response and encourages participation.
             </div>
+            
+            
+            
         </div>
     )
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,8 +5,8 @@ export default function Topbar({menuOpen,setMenuOpen}) {
         <div className={"topbar" + (menuOpen && "active")} id="topbar">
             <div className="wrapper">
                 <a href="#intro" className="name">Gemcila Samini Gino Charlton</a>
-                <a href="#aboutpage" className="about">About</a>
-               <div className="myworks" onClick={()=>setMenuOpen(!menuOpen)}>Works</div>
+                <a href="#aboutpage" className="about">About Me</a>
+               <div className="myworks" onClick={()=>setMenuOpen(!menuOpen)}>Projects</div>
             </div>
         </div>
     )
