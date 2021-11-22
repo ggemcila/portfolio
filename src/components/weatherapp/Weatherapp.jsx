de54e14f8ee110040e648750396e29f6139b2b96
Gemcila <ggemcila@gmail.com>
2021-11-22 15:47:18 +0000

    #21 Layout - weatherapp page

--- src/components/weatherapp/Weatherapp.jsx
+++ src/components/weatherapp/Weatherapp.jsx
@@ -1,9 +1,18 @@
 import "./weatherapp.scss";
+import weathertoday from "../../assets/weathertoday.png";
+import sky from "../../assets/sky.jpg";
 
 export default function Weatherapp() {
     return (
+        <div className="containerweather"> 
         <div className="weatherapp" id="weatherapp">
-            
+           <img src={weathertoday} className="weathertoday"></img>  
         </div>
+        <div className="weatherinfo">
+            <img src={sky} className="sky"></img>
+        </div>
+        </div>
+        
+        
     )
 }
