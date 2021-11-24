208f27eba6e06b952843d7fbd89b812c14f64057
Gemcila <ggemcila@gmail.com>
2021-11-24 20:42:12 +0000

    #27 Layout - Game review app

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,11 @@ function App() {
      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
      <div className="sections">
+     
        <Intro/>
-       <Weatherapp/>
        <Quizapp/>
+       <Weatherapp/>
        <Gamesreview/>
-       
        <About/>
      </div>
     </div>
--- src/components/gamesreview/Gamesreview.jsx
+++ src/components/gamesreview/Gamesreview.jsx
@@ -3,7 +3,9 @@ import "./gamesreview.scss";
 export default function Gamesreview() {
     return (
         <div className="gamesreview" id="gamesreview">
-            
+            <div className="screen">
+              <h1 className="title3">Game review app</h1>
+            </div>
         </div>
     )
 }
