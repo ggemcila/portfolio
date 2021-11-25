6832c89d96e5cbbe30e39cd2040889df3d2255e9
Gemcila <ggemcila@gmail.com>
2021-11-25 10:01:35 +0000

    change of order of components in the menubar

--- src/components/menuworks/Menu.jsx
+++ src/components/menuworks/Menu.jsx
@@ -3,12 +3,13 @@ import "./menu.scss";
 function Menu({menuOpen,setMenuOpen}){ //pass props
     return(
         <div className={"menu " + (menuOpen && "active")} id="menu">
-            <ul>
-                <li onClick={()=>setMenuOpen(false)}>
-                    <a href="#weatherapp">Weather app</a>
-                </li>
+            <ul> 
                 <li onClick={()=>setMenuOpen(false)}>
                     <a href="#quizapp">Quiz app</a>
+                </li>
+                
+                <li onClick={()=>setMenuOpen(false)}>
+                    <a href="#weatherapp">Weather app</a>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
                     <a href="#gamesreview">Games review app</a>
